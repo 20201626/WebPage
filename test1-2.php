47dd0c6d8d97a5b8520d3d1fa83ae2be64327067
20201626 <127705875+20201626@users.noreply.github.com>
2023-04-17 09:07:09 +0900

    Add files via upload

---
+++ test1-2.php
@@ -0,0 +1,25 @@
+<DOCTYPE html>
+<html>
+<body>
+
+<?php
+$n = 30;
+$sum = 1;
+$prod = 1;
+for($i=  1; $i <= $n; $i++) {
+    echo $i;
+    echo "<br>";
+    $sum += $i;
+    $prod = $prod * $i;
+
+}
+
+echo $sum;
+echo "<br>";
+echo $prod;
+echo "<br>";
+
+?>
+
+</body>
+</html>
